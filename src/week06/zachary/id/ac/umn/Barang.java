0a2fabef357797dbb0be7556b0ec890bbcc7e4bf
Cazhed <zachary.sechan@gmail.com>
2025-10-07 20:22:20 +0700

    Barang

---
+++ src/week06/zachary/id/ac/umn/Barang.java
@@ -0,0 +1,39 @@
+package week06.zachary.id.ac.umn;
+
+public class Barang {
+	private int id;
+	private double harga;
+	private String nama;
+	private int stok;
+	
+	public Barang(int id, String nama, double harga) {
+		this.id = id;
+		this.nama = nama;
+		this.harga = harga;
+		this.stok = 0;
+	}
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public String getNama() {
+		return this.nama;
+	}
+	
+	public double getHarga() {
+		return this.harga;
+	}
+	
+	public int getStok() {
+		return this.stok;
+	}
+	
+	public void minusStok(int jml) {
+		if (this.stok >= jml) {
+			this.stok -= jml;
+		} else {
+			System.out.println("Stok habis.");
+		}
+	}
+}
